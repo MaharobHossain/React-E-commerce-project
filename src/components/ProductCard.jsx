081fc96fe2332e7aa2f651a05b2b542c6a4a0fa0
maharob888 <maharobhossain8880@gmail.com>
2023-12-25 13:42:34 +0600

    OrderList Update And Product Card Update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,61 +2,79 @@ import React from 'react'
 import { useCart } from 'react-use-cart';
 import Toaster from './common/Toaster';
 import '../assets/css/ProductCard.css'
+import { Link } from 'react-router-dom';
 
 
 const ProductCard = (props) => {
-const {item} = props;
-const {addItem} = useCart();
-
-
-
-
-
-    console.log(item);
-    return (
-    <div >
-     
-      
-            <div className='rounded-3xl shadow-2xl bg-gray-50   '>
-            <div className='rounded-3xl shadow-2xl  overflow-hidden  product-cart ' >        
-
-
-                     <img src={item.image.small} alt="" />
-                     </div>
-                     <div className='text-2xl font-medium mt-2 text-center	'>
-                     <h1 >{item.name}</h1>
-                     </div>
-                     <div className='text-center font-mono'>
-                     <h1>{item.formatted_regular_price}</h1>
-                     </div>
-                     <div className='text-center font-mono'>
-                     <h1>{item.discount}</h1>
-                     </div>
-                     <div className='text-center font-mono'>
-                      <h1>{item.discounted_price}</h1>
-                     </div>
-                     <div className='text-right'>
-                     <button className='bg-yellow-400 text-black p-3 rounded-lg hover:bg-black hover:text-white' onClick={(e)=>{
-                      e.preventDefault();
-                      addItem({
-                      id:item?.id,
-                      product_id: item?.id,
-                      name:item?.name,
-                      price:item?.final_product_price,
-                      image:item?.image?.small,
-                      stock:item?.stock
-                      },1);
-                      Toaster("Product Add to cart", 'success')
-                      }}> Add to curt</button>
-
-
-
-                      </div>
-                      </div>
-                     
-     
-      </div>
-  )
-  }
-
-export default ProductCard;
+  const {item} = props;
+  const {addItem} = useCart();
+  const {product} = props;
+  
+  
+  
+  
+      console.log(item);
+      return (
+      <div >
+       
+       <Link to={`product/${item?.slug}`}>
+  
+       <div className='rounded-3xl shadow-2xl bg-gray-50   '>
+              <div className='rounded-3xl shadow-2xl  overflow-hidden  product-cart ' >        
+  
+             {Number(item?.discounted_price)>0 &&(
+              <div className='bg-black text-center text-white w-20 '>{item?.discount} Off</div>
+             )}
+  
+            {Number(item?.stock)<1 && (
+                <div className='bg-red-700 text-right text-black w-20 '>Out of stock</div>
+            )}
+  
+                       <img src={item.image.small} alt="" />
+                       </div>
+                       <div className='text-2xl font-medium mt-2 text-center	'>
+                       <h1 >{item.name}</h1>
+                       </div>
+                       <div className='text-center font-mono'>
+                       <h1>{item.formatted_regular_price}</h1>
+                       </div>
+                       <div className='text-center font-mono'>
+                       <h1>{item.discount}</h1>
+                       </div>
+                       <div className='text-center font-mono'>
+                        <h1>{item.discounted_price}</h1>
+                       </div>
+                       <div className='text-right'>
+                       <button className='bg-yellow-400 text-black p-3 rounded-lg hover:bg-black hover:text-white' onClick={(e)=>{
+                        e.preventDefault();
+                        if(Number(item?.stock)>0){
+                          addItem({
+                              id: item?.id,
+                              product_id: item?.id,
+                              name: item?.name,
+                              slug: item?.slug,
+                              price: item?.final_product_price,
+                              image: item?.image?.small,
+                              stock: item?.stock
+                          });
+                         Toaster('Product added to cart', 'success');
+                      }
+                      else{
+                          Toaster('Product is out of stock', 'warn')
+                      }
+                        }}> Add to curt</button>
+  
+  
+  
+                        </div>
+                        </div>
+                       
+         
+       </Link>
+        
+                 
+        </div>
+    )
+    }
+  
+  export default ProductCard;
--- src/pages/user/OrderDetails.jsx
+++ src/pages/user/OrderDetails.jsx
@@ -39,13 +39,23 @@ const OrderDetails = () => {
           <Layout>
 
               <DashboardLayout>
-                  <h3 className="fw-bold text-xl">Order details {order?.order_number}</h3>
+                  <h3 className="fw-bold font-semibold text-xl">Order Details : </h3>
+                  <br/>
+                  <h3 className="fw-bold font-mono text-xl">Order Number : {order?.order_number}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Time : {order?.time}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Shiping Name : {order?.shipping_name}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Shiping Phone : {order?.shipping_phone}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Shiping Email : {order?.shipping_email}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Shiping Address : {order?.shipping_address}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Status : {order?.status}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Discount : {order?.discount}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Total Pay Ammount : {order?.pay_amount}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Payment Method :{order?.payment_method}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Payment Status :{order?.payment_status}</h3>
+                  <h3 className="fw-bold font-mono text-xl">Order Shiping Charge : {order?.shipping_charges}</h3>
 
-                {order?.time}
-                {order?.time}
-                {order?.time}
-                {order?.time}
-                {order?.time}
+                
+
 
                 {/* {order?.order_details.map()} */}
 
