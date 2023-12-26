3db2c543f3f3fb80e6b5b5e8926bd197e4487a9f
maharob888 <maharobhossain8880@gmail.com>
2023-12-27 00:13:04 +0600

    MobileMenu Update

--- src/layouts/MobileMenu.jsx
+++ src/layouts/MobileMenu.jsx
@@ -96,6 +96,14 @@ function MobileMenu() {
                              Log-In
                             </Link>
                            </li>
+
+                           <li className='text-lg text-black transition-colors hover:text-green-600 '>
+                            <Link to='/user/dashbord'  className={window.location.pathname === '/' ? 'mobileMenudActive' : ''} >
+                            Dashboard
+                            </Link>
+                           </li>
+
+
                 </ul>
               </div>
             </Dialog.Panel>
